d524a9378d5d92bbdcff8a2963c8bd2f69486370
amkondal <amitkondal203@gmail.com>
2022-04-04 14:38:35 +0530

    Updated

--- src/main/java/com/abn/amro/demo/config/SecurityConfiguration.java
+++ src/main/java/com/abn/amro/demo/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/h2-console/**", "/swagger-ui/**", "/abn-amro-assignment/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().hasRole("ADMIN")
                 .and()
                 .httpBasic();
     }
@@ -31,13 +31,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .and()
                 .withUser("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN");
+                .roles("ADMIN");
     }
 
     @Bean
--- src/main/java/com/abn/amro/demo/dto/RecipeDTO.java
+++ src/main/java/com/abn/amro/demo/dto/RecipeDTO.java
@@ -8,7 +8,7 @@ import com.abn.amro.demo.entity.IngredientEntity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Date;
 import java.util.List;
@@ -17,16 +17,16 @@ import java.util.List;
 public class RecipeDTO {
 
     private Long id;
-    @NotEmpty(message = "Recipe Name is required")
-    @Size(min = 1, max = 50, message = "Recipe name must be less than 50")
+    @NotNull(message = "Recipe Name is required")
+    @Size(min = 1, max = 50, message = "Recipe name must be of length 1 to 50")
     private String name;
-    @NotEmpty(message = "Recipe Ingredients required")
+    @NotNull(message = "Recipe Ingredients required")
     private List<IngredientEntity> ingredients;
-    @NotEmpty(message = "Recipe Cooking instructions are required")
-    @Size(min = 1, max = 1000, message = "Recipe Cooking instructions must be less than 1000")
+    @NotNull(message = "Recipe Cooking instructions are required")
+    @Size(min = 1, max = 200, message = "Recipe Cooking instructions must be of length 1 to 200")
     private String cookingInstructions;
-    @NotEmpty(message = "Recipe type is required.")
-    @Size(min = 1, max = 20, message = "Recipe type must be less than 20")
+    @NotNull(message = "Recipe type is required.")
+    @Size(min = 1, max = 10, message = "Recipe type must be of length 1 to 10")
     private String recipeType;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd‐MM‐yyyy HH:mm")
     private Date createdAt;
